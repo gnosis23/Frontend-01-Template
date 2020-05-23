18df0fe078f0e3a42635a2e348fc8ca5bb65d326
Bohao Wang <bj050323@gmail.com>
2020-05-23 17:44:14 +0800

    feat: layout step 4 计算主轴、纵轴

--- browser/project/layout.js
+++ browser/project/layout.js
@@ -2,6 +2,8 @@
  * flex layout
  */
 
+const isNotBlank = value => value !== null && value !== undefined;
+
 /**
  * 获取style属性，必要时初始化
  * @param element
@@ -188,7 +190,180 @@ function layout(element) {
     }
   }
   flexLine.mainSpace = mainSpace;
-  // 漏了crossSpace？
+
+  if (style.flexWrap === 'nowrap' || isAutoMainSize) {
+    flexLine.crossSpace = (style[crossSize] !== undefined) ? style[crossSize] : crossSpace;
+  } else {
+    flexLine.crossSpace = crossSpace;
+  }
+
+  if (mainSpace < 0) {
+    // overflow (happens only if container is single line), scale every item
+    let scale = style[mainSize] / (style[mainSize] - mainSpace);
+    let currentMain = mainBase;
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      let itemStyle = getStyle(item);
+
+      if (itemStyle.flex) {
+        itemStyle[mainSize] = 0;
+      }
+
+      itemStyle[mainSize] = itemStyle[mainSize] * scale;
+
+      itemStyle[mainStart] = currentMain;
+      itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
+      currentMain = itemStyle[mainEnd];
+    }
+  } else {
+    flexLines.forEach(items => {
+      let mainSpace = items.mainSpace;
+      let flexTotal = 0;
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let itemStyle = getStyle(item);
+
+        if ((itemStyle.flex !== null) && (itemStyle.flex !== undefined)) {
+          flexTotal += itemStyle.flex;
+          // continue;
+        }
+      }
+
+      if (flexTotal > 0) {
+        // There is flexible items
+        let currentMain = mainBase;
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i];
+          let itemStyle = getStyle(item);
+
+          if (itemStyle.flex) {
+            itemStyle[mainSize] = (mainSpace / flexTotal) * itemStyle.flex;
+          }
+          itemStyle[mainStart] = currentMain;
+          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
+          currentMain = itemStyle[mainEnd];
+        }
+      } else {
+        // There is *NO* flexible flex items, which means, justifyContent should work
+        let currentMain, step
+        if (style.justifyContent === 'flex-start') {
+          currentMain = mainBase;
+          step = 0;
+        }
+        if (style.justifyContent === 'flex-end') {
+          currentMain = mainSpace * mainSign + mainBase;
+          step = 0;
+        }
+        if (style.justifyContent === 'center') {
+          currentMain = mainSpace / 2 * mainSign + mainBase;
+          step = 0;
+        }
+        if (style.justifyContent === 'space-between') {
+          step = mainSpace / (items.length - 1) * mainSign;
+          currentMain = mainBase;
+        }
+        if (style.justifyContent === 'space-around') {
+          step = mainSpace / items.length * mainSign;
+          currentMain = step / 2 + mainBase;
+        }
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i];
+          const itemStyle = getStyle(item);
+          itemStyle[mainStart] = currentMain;
+          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
+          currentMain = itemStyle[mainEnd] + step;
+        }
+      }
+    });
+  }
+
+  // compute the cross axis sizes
+  // align-items, align-self
+  let restCrossSpace = 0;
+  if (!style[crossSize]) {  // auto sizing
+    restCrossSpace = 0;
+    elementStyle[crossSize] = 0;
+    for (let i = 0; i < flexLines.length; i++) {
+      elementStyle[crossSize] = elementStyle[crossSize] + flexLines[i].crossSpace;
+    }
+  }
+  else {
+    restCrossSpace = style[crossSize];
+    for (let i = 0; i < flexLines.length; i++) {
+      restCrossSpace -= flexLines[i].crossSpace;
+    }
+  }
+
+  if (style.flexWrap === 'wrap-reverse') {
+    crossBase = style[crossSize];
+  } else {
+    crossBase = 0;
+  }
+  // let lineSize = style[crossSize] / flexLines.length;
+
+  let step;
+  if (style.alignContent === 'flex-start') {
+    crossBase += 0;
+    step = 0;
+  }
+  if (style.alignContent === 'flex-end') {
+    crossBase += crossSign * restCrossSpace;
+    step = 0;
+  }
+  if (style.alignContent === 'center') {
+    crossBase += crossSign * restCrossSpace / 2;
+    step = 0;
+  }
+  if (style.alignContent === 'space-between') {
+    crossBase += 0;
+    step = restCrossSpace / (flexLines.length - 1);
+  }
+  if (style.alignContent === 'space-around') {
+    step = restCrossSpace / (flexLines.length - 1);
+    crossBase += crossSign * step / 2;
+  }
+  if (style.alignContent === 'stretch') {
+    crossBase += 0;
+    step = 0;
+  }
+
+  flexLines.forEach(items => {
+    // https://drafts.csswg.org/css-flexbox-1/#align-content-property
+    let lineCrossSize = style.alignContent === 'stretch'
+      ? items.crossSpace + restCrossSpace / flexLines.length // 瓜分剩余
+      : items.crossSpace;
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      let itemStyle = getStyle(item);
+
+      let align = itemStyle.alignSelf || style.alignItems;
+
+      if (itemStyle[crossSize] === null || itemStyle[crossSize] === undefined) {
+        itemStyle[crossSize] = (align === 'stretch') ? lineCrossSize : 0;
+      }
+
+      if (align === 'flex-start') {
+        itemStyle[crossStart] = crossBase;
+        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
+      }
+      if (align === 'flex-end') {
+        itemStyle[crossEnd] = crossBase + crossSign * lineCrossSize;
+        itemStyle[crossStart] = itemStyle[crossEnd] - crossSign * itemStyle[crossSize];
+      }
+      if (align === 'center') {
+        itemStyle[crossStart] = crossBase + crossSign * (lineCrossSize - itemStyle[crossSize]) / 2;
+        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
+      }
+      if (align === 'stretch') {
+        itemStyle[crossStart] = crossBase;
+        itemStyle[crossEnd] = crossBase
+          + crossSign * (isNotBlank(itemStyle[crossSize])? itemStyle[crossSize] : lineCrossSize);
+        itemStyle[crossSize] = crossSign * (itemStyle[crossEnd] - itemStyle[crossStart]);
+      }
+    }
+    crossBase += crossSign * (lineCrossSize + step);
+  });
+
 }
 
 module.exports = layout;
