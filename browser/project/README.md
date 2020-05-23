d0eef950a0d2f9a9b02aaf29e7760ac62da11a35
Bohao Wang <bj050323@gmail.com>
2020-05-24 00:38:41 +0800

    feat: svg adaptor

---
+++ browser/project/README.md
@@ -0,0 +1,29 @@
+# toy browser
+玩具浏览器，展示浏览器渲染流程
+
+## 运行
+```shell
+npm install
+# 打开服务器
+node ./server 
+# 打开客户端
+node .client
+# 浏览器打开 build/viewport.svg
+```
+
+## 文件结构
+```
+.
+├── build           # 输出文件
+├── doc             # 相关组件文档
+├── test
+├── adaptor         # 渲染器适配
+├── client.js       # 浏览器客户端
+├── css.js          # 样式计算
+├── layout.js       # 布局计算
+├── parser.js       # html解析
+├── render.js       # 渲染器
+├── selector.js     # css选择器解析
+├── server.js       # 服务器
+
+```
--- browser/project/render.js
+++ browser/project/render.js
@@ -2,65 +2,22 @@
  * render
  *
  */
-const fs = require('fs');
-const Path = require('path');
-
-class SvgViewPort {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.children = [];
-  }
-  addToCanvas(img, left, top) {
-    if (!img.width || !img.height) return;
-    img.left = left;
-    img.top = top;
-    this.children.push(img);
-  }
-  draw(item) {
-    let text = `<rect width="${item.width}" height="${item.height}" fill="${item.color}"`;
-    text += ` x="${item.left}" y="${item.top}"`;
-    text += ' />\n'
-    return text;
-  }
-  save(path) {
-    let text = `<svg version="1.1" width="${this.width}" height="${this.height}" xmlns="http://www.w3.org/2000/svg">\n`;
-    this.children.forEach(node => {
-      text += this.draw(node);
-    });
-    text += `</svg>`;
-    // console.log(text);
-    fs.writeFileSync(Path.join('./build/', path), text);
-  }
-}
-
-class Rect {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.color = '';
-  }
-  fill(r, g, b) {
-    this.color = `rgb(${r},${g},${b})`;
-  }
-}
+const SvgRender = require('./adaptor/svg');
+const Adaptor = SvgRender;
 
 function createViewPort(width, height) {
-  return new SvgViewPort(width, height);
-}
-
-function createImage(width, height) {
-  return new Rect(width, height);
+  return new Adaptor(width, height);
 }
 
 function render(viewport, element) {
   if (element.style) {
-    let img = createImage(element.style.width, element.style.height);
-
-    let color = element.style['backgroundColor'] || 'rgb(0,0,0)';
-    color.match(/rgb\((\d+),(\d+),(\d+)\)/);
-    img.fill(Number(RegExp.$1), Number(RegExp.$2), Number(RegExp.$3));
-    viewport.addToCanvas(img, element.style.left || 0, element.style.top || 0);
+    viewport.addToCanvas(
+      element.style.width,
+      element.style.height,
+      element.style.left || 0,
+      element.style.top || 0,
+      element.style['backgroundColor']
+    );
   }
 
   if (element.children) {
