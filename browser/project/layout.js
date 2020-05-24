dfb1ca908bff4e531d021e52f21668ec5760e98c
Bohao Wang <bj050323@gmail.com>
2020-05-24 21:55:28 +0800

    test: 主轴计算

--- browser/project/layout.js
+++ browser/project/layout.js
@@ -98,6 +98,185 @@ function putInLines(items, elementStyle, isAutoMainSize, mainSize, crossSize) {
   return { flexLines, mainSpace };
 }
 
+/**
+ * 主轴大小和位置计算
+ * @param items
+ * @param style
+ * @param flexLines
+ * @param mainSpace
+ * @param mainInfo
+ */
+function mainSizeDetermination(items, style, flexLines, mainSpace, mainInfo) {
+  const { mainSize, mainStart, mainEnd, mainBase, mainSign } = mainInfo;
+  if (mainSpace < 0) {
+    // overflow (happens only if container is single line), scale every item
+    let scale = style[mainSize] / (style[mainSize] - mainSpace);
+    let currentMain = mainBase;
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      let itemStyle = getStyle(item);
+
+      if (itemStyle.flex) {
+        itemStyle[mainSize] = 0;
+      }
+
+      itemStyle[mainSize] = itemStyle[mainSize] * scale;
+
+      itemStyle[mainStart] = currentMain;
+      itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
+      currentMain = itemStyle[mainEnd];
+    }
+  } else {
+    flexLines.forEach(items => {
+      let mainSpace = items.mainSpace;
+      let flexTotal = 0;
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let itemStyle = getStyle(item);
+
+        if ((itemStyle.flex !== null) && (itemStyle.flex !== undefined)) {
+          flexTotal += itemStyle.flex;
+          // continue;
+        }
+      }
+
+      if (flexTotal > 0) {
+        // There is flexible items
+        let currentMain = mainBase;
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i];
+          let itemStyle = getStyle(item);
+
+          if (itemStyle.flex) {
+            itemStyle[mainSize] = (mainSpace / flexTotal) * itemStyle.flex;
+          }
+          itemStyle[mainStart] = currentMain;
+          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
+          currentMain = itemStyle[mainEnd];
+        }
+      } else {
+        // There is *NO* flexible flex items, which means, justifyContent should work
+        let currentMain, step
+        if (style.justifyContent === 'flex-start') {
+          currentMain = mainBase;
+          step = 0;
+        }
+        if (style.justifyContent === 'flex-end') {
+          currentMain = mainSpace * mainSign + mainBase;
+          step = 0;
+        }
+        if (style.justifyContent === 'center') {
+          currentMain = mainSpace / 2 * mainSign + mainBase;
+          step = 0;
+        }
+        if (style.justifyContent === 'space-between') {
+          step = mainSpace / (items.length - 1) * mainSign;
+          currentMain = mainBase;
+        }
+        if (style.justifyContent === 'space-around') {
+          step = mainSpace / items.length * mainSign;
+          currentMain = step / 2 + mainBase;
+        }
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i];
+          const itemStyle = getStyle(item);
+          itemStyle[mainStart] = currentMain;
+          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
+          currentMain = itemStyle[mainEnd] + step;
+        }
+      }
+    });
+  }
+}
+
+function crossSizeDetermine(items, style, flexLines, crossInfo) {
+  let { crossSize, crossBase, crossStart, crossEnd, crossSign } = crossInfo;
+  let restCrossSpace = 0;
+  if (!style[crossSize]) {  // auto sizing
+    restCrossSpace = 0;
+    style[crossSize] = 0;
+    for (let i = 0; i < flexLines.length; i++) {
+      style[crossSize] = style[crossSize] + flexLines[i].crossSpace;
+    }
+  }
+  else {
+    restCrossSpace = style[crossSize];
+    for (let i = 0; i < flexLines.length; i++) {
+      restCrossSpace -= flexLines[i].crossSpace;
+    }
+  }
+
+  if (style.flexWrap === 'wrap-reverse') {
+    crossBase = style[crossSize];
+  } else {
+    crossBase = 0;
+  }
+  // let lineSize = style[crossSize] / flexLines.length;
+
+  let step;
+  if (style.alignContent === 'flex-start') {
+    crossBase += 0;
+    step = 0;
+  }
+  if (style.alignContent === 'flex-end') {
+    crossBase += crossSign * restCrossSpace;
+    step = 0;
+  }
+  if (style.alignContent === 'center') {
+    crossBase += crossSign * restCrossSpace / 2;
+    step = 0;
+  }
+  if (style.alignContent === 'space-between') {
+    crossBase += 0;
+    step = restCrossSpace / (flexLines.length - 1);
+  }
+  if (style.alignContent === 'space-around') {
+    step = restCrossSpace / (flexLines.length - 1);
+    crossBase += crossSign * step / 2;
+  }
+  if (style.alignContent === 'stretch') {
+    crossBase += 0;
+    step = 0;
+  }
+
+  flexLines.forEach(items => {
+    // https://drafts.csswg.org/css-flexbox-1/#align-content-property
+    let lineCrossSize = style.alignContent === 'stretch'
+      ? items.crossSpace + restCrossSpace / flexLines.length // 瓜分剩余
+      : items.crossSpace;
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      let itemStyle = getStyle(item);
+
+      let align = itemStyle.alignSelf || style.alignItems;
+
+      if (itemStyle[crossSize] === null || itemStyle[crossSize] === undefined) {
+        itemStyle[crossSize] = (align === 'stretch') ? lineCrossSize : 0;
+      }
+
+      if (align === 'flex-start') {
+        itemStyle[crossStart] = crossBase;
+        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
+      }
+      if (align === 'flex-end') {
+        itemStyle[crossEnd] = crossBase + crossSign * lineCrossSize;
+        itemStyle[crossStart] = itemStyle[crossEnd] - crossSign * itemStyle[crossSize];
+      }
+      if (align === 'center') {
+        itemStyle[crossStart] = crossBase + crossSign * (lineCrossSize - itemStyle[crossSize]) / 2;
+        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
+      }
+      if (align === 'stretch') {
+        itemStyle[crossStart] = crossBase;
+        itemStyle[crossEnd] = crossBase
+          + crossSign * (isNotBlank(itemStyle[crossSize])? itemStyle[crossSize] : lineCrossSize);
+        itemStyle[crossSize] = crossSign * (itemStyle[crossEnd] - itemStyle[crossStart]);
+      }
+    }
+    crossBase += crossSign * (lineCrossSize + step);
+  });
+}
+
 function layout(element) {
   if (!element.computedStyle)
     return;
@@ -215,177 +394,19 @@ function layout(element) {
   const { flexLines, mainSpace } = putInLines(items, elementStyle, isAutoMainSize, mainSize, crossSize);
 
   // 3 确定主轴大小
-  if (mainSpace < 0) {
-    // overflow (happens only if container is single line), scale every item
-    let scale = style[mainSize] / (style[mainSize] - mainSpace);
-    let currentMain = mainBase;
-    for (let i = 0; i < items.length; i++) {
-      let item = items[i];
-      let itemStyle = getStyle(item);
-
-      if (itemStyle.flex) {
-        itemStyle[mainSize] = 0;
-      }
-
-      itemStyle[mainSize] = itemStyle[mainSize] * scale;
-
-      itemStyle[mainStart] = currentMain;
-      itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
-      currentMain = itemStyle[mainEnd];
-    }
-  } else {
-    flexLines.forEach(items => {
-      let mainSpace = items.mainSpace;
-      let flexTotal = 0;
-      for (let i = 0; i < items.length; i++) {
-        let item = items[i];
-        let itemStyle = getStyle(item);
-
-        if ((itemStyle.flex !== null) && (itemStyle.flex !== undefined)) {
-          flexTotal += itemStyle.flex;
-          // continue;
-        }
-      }
-
-      if (flexTotal > 0) {
-        // There is flexible items
-        let currentMain = mainBase;
-        for (let i = 0; i < items.length; i++) {
-          let item = items[i];
-          let itemStyle = getStyle(item);
-
-          if (itemStyle.flex) {
-            itemStyle[mainSize] = (mainSpace / flexTotal) * itemStyle.flex;
-          }
-          itemStyle[mainStart] = currentMain;
-          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
-          currentMain = itemStyle[mainEnd];
-        }
-      } else {
-        // There is *NO* flexible flex items, which means, justifyContent should work
-        let currentMain, step
-        if (style.justifyContent === 'flex-start') {
-          currentMain = mainBase;
-          step = 0;
-        }
-        if (style.justifyContent === 'flex-end') {
-          currentMain = mainSpace * mainSign + mainBase;
-          step = 0;
-        }
-        if (style.justifyContent === 'center') {
-          currentMain = mainSpace / 2 * mainSign + mainBase;
-          step = 0;
-        }
-        if (style.justifyContent === 'space-between') {
-          step = mainSpace / (items.length - 1) * mainSign;
-          currentMain = mainBase;
-        }
-        if (style.justifyContent === 'space-around') {
-          step = mainSpace / items.length * mainSign;
-          currentMain = step / 2 + mainBase;
-        }
-        for (let i = 0; i < items.length; i++) {
-          let item = items[i];
-          const itemStyle = getStyle(item);
-          itemStyle[mainStart] = currentMain;
-          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize];
-          currentMain = itemStyle[mainEnd] + step;
-        }
-      }
-    });
-  }
+  const mainInfo = { mainSize, mainBase, mainStart, mainEnd, mainSign };
+  mainSizeDetermination(items, style, flexLines, mainSpace, mainInfo);
 
   // 4 确定纵轴大小
   // align-items, align-self
-  let restCrossSpace = 0;
-  if (!style[crossSize]) {  // auto sizing
-    restCrossSpace = 0;
-    elementStyle[crossSize] = 0;
-    for (let i = 0; i < flexLines.length; i++) {
-      elementStyle[crossSize] = elementStyle[crossSize] + flexLines[i].crossSpace;
-    }
-  }
-  else {
-    restCrossSpace = style[crossSize];
-    for (let i = 0; i < flexLines.length; i++) {
-      restCrossSpace -= flexLines[i].crossSpace;
-    }
-  }
-
-  if (style.flexWrap === 'wrap-reverse') {
-    crossBase = style[crossSize];
-  } else {
-    crossBase = 0;
-  }
-  // let lineSize = style[crossSize] / flexLines.length;
-
-  let step;
-  if (style.alignContent === 'flex-start') {
-    crossBase += 0;
-    step = 0;
-  }
-  if (style.alignContent === 'flex-end') {
-    crossBase += crossSign * restCrossSpace;
-    step = 0;
-  }
-  if (style.alignContent === 'center') {
-    crossBase += crossSign * restCrossSpace / 2;
-    step = 0;
-  }
-  if (style.alignContent === 'space-between') {
-    crossBase += 0;
-    step = restCrossSpace / (flexLines.length - 1);
-  }
-  if (style.alignContent === 'space-around') {
-    step = restCrossSpace / (flexLines.length - 1);
-    crossBase += crossSign * step / 2;
-  }
-  if (style.alignContent === 'stretch') {
-    crossBase += 0;
-    step = 0;
-  }
-
-  flexLines.forEach(items => {
-    // https://drafts.csswg.org/css-flexbox-1/#align-content-property
-    let lineCrossSize = style.alignContent === 'stretch'
-      ? items.crossSpace + restCrossSpace / flexLines.length // 瓜分剩余
-      : items.crossSpace;
-    for (let i = 0; i < items.length; i++) {
-      let item = items[i];
-      let itemStyle = getStyle(item);
-
-      let align = itemStyle.alignSelf || style.alignItems;
-
-      if (itemStyle[crossSize] === null || itemStyle[crossSize] === undefined) {
-        itemStyle[crossSize] = (align === 'stretch') ? lineCrossSize : 0;
-      }
-
-      if (align === 'flex-start') {
-        itemStyle[crossStart] = crossBase;
-        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
-      }
-      if (align === 'flex-end') {
-        itemStyle[crossEnd] = crossBase + crossSign * lineCrossSize;
-        itemStyle[crossStart] = itemStyle[crossEnd] - crossSign * itemStyle[crossSize];
-      }
-      if (align === 'center') {
-        itemStyle[crossStart] = crossBase + crossSign * (lineCrossSize - itemStyle[crossSize]) / 2;
-        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
-      }
-      if (align === 'stretch') {
-        itemStyle[crossStart] = crossBase;
-        itemStyle[crossEnd] = crossBase
-          + crossSign * (isNotBlank(itemStyle[crossSize])? itemStyle[crossSize] : lineCrossSize);
-        itemStyle[crossSize] = crossSign * (itemStyle[crossEnd] - itemStyle[crossStart]);
-      }
-    }
-    crossBase += crossSign * (lineCrossSize + step);
-  });
+  const crossInfo = { crossSize, crossBase, crossStart, crossEnd, crossSign } ;
+  crossSizeDetermine(items, style, flexLines, crossInfo);
 
   // console.log(flexLines);
 }
 
 module.exports = {
   putInLines,
+  mainSizeDetermination,
   layout
 };
--- browser/project/test/layout.test.js
+++ browser/project/test/layout.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const { putInLines } = require('../layout');
+const { putInLines, mainSizeDetermination } = require('../layout');
 
 const toDesc = item => {
   return Object.keys(item).reduce((obj, key) => {
@@ -53,4 +53,77 @@ describe('layout', () => {
     expect(flexLines[0].crossSpace).to.equal(250);
     expect(flexLines[1].crossSpace).to.equal(200);
   });
+
+  it('should decide width', () => {
+    const items = buildItems([
+      { width: '500px', height: '666px' },
+      { width: '500px' },
+      { width: '1000px' },
+    ]);
+    const elementStyle = {
+      width: 1000,
+      flexWrap: 'nowrap',
+      height: 500
+    };
+
+    const { flexLines, mainSpace } = putInLines(items, elementStyle, false, 'width', 'height');
+
+    const mainInfo = {
+      mainSize: 'width',
+      mainStart: 'left',
+      mainEnd: 'right',
+      mainBase: 0,
+      mainSign: 1
+    };
+    mainSizeDetermination(items, elementStyle, flexLines, mainSpace, mainInfo);
+    expect(items[0].style.width).to.equal(250);
+    expect(items[0].style.left).to.equal(0);
+    expect(items[0].style.right).to.equal(250);
+    expect(items[1].style.width).to.equal(250);
+    expect(items[1].style.left).to.equal(250);
+    expect(items[1].style.right).to.equal(500);
+    expect(items[2].style.width).to.equal(500);
+    expect(items[2].style.left).to.equal(500);
+    expect(items[2].style.right).to.equal(1000);
+  });
+
+  it('should decide width in two lines', () => {
+    const items = buildItems([
+      { width: '400px' },
+      { width: '400px', height: '200px' },
+      { flex: 1, height: '250px' },
+      { width: '2000px', height: '200px' },
+    ]);
+    const elementStyle = {
+      width: 1000,
+      flexWrap: 'wrap',
+      height: 500,
+      justifyContent: 'flex-end'
+    };
+
+    const { flexLines, mainSpace } = putInLines(items, elementStyle, false, 'width', 'height');
+    const mainInfo = {
+      mainSize: 'width',
+      mainStart: 'left',
+      mainEnd: 'right',
+      mainBase: 0,
+      mainSign: 1
+    };
+    mainSizeDetermination(items, elementStyle, flexLines, mainSpace, mainInfo);
+
+    expect(items[0].style.width).to.equal(400);
+    expect(items[0].style.left).to.equal(0);
+    expect(items[0].style.right).to.equal(400);
+    expect(items[1].style.width).to.equal(400);
+    expect(items[1].style.left).to.equal(400);
+    expect(items[1].style.right).to.equal(800);
+    expect(items[2].style.width).to.equal(200);
+    expect(items[2].style.left).to.equal(800);
+    expect(items[2].style.right).to.equal(1000);
+    // 算法里没有收缩所以是2000
+    expect(items[3].style.width).to.equal(2000);
+    expect(items[3].style.left).to.equal(0);
+    expect(items[3].style.right).to.equal(2000);
+  });
+
 });
