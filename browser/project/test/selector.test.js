e17f120a264adb63896cc8a1f99d54ca99818861
Bohao Wang <bj050323@gmail.com>
2020-05-24 19:49:15 +0800

    misc: update the test framework to Mocha

--- browser/project/test/selector.test.js
+++ browser/project/test/selector.test.js
@@ -1,15 +1,25 @@
+const expect = require('chai').expect;
 const { Lexer, parseSelector } = require('../selector')
 
-const expect = (a, b) => { if (a !== b) console.error(a, b) }
+const selector = "div.mainWrapper SPAN>a#id.cls[href=\"www.baidu.com\"]"
 
-const selector = "div.mainWapper SPAN>a#id.cls[href=\"www.baidu.com\"]"
-const lexer = new Lexer()
-const tokens = lexer.tokens(selector)
-console.log(tokens);
-expect(
-  tokens.filter(x => x.type === 'ident').map(x => x.text).join(' '),
-  'div mainWrapper span a cls href'
-)
+describe('selector', () => {
+  describe('lexer tokens', () => {
+    it('should return', () => {
+      const lexer = new Lexer()
+      const tokens = lexer.tokens(selector)
+      expect(tokens.filter(x => x.type === 'ident').map(x => x.text).join(' ')).to.equal('div mainWrapper SPAN a cls href')
+    });
+  });
 
-const selectors = parseSelector(selector);
-console.log(JSON.stringify(selectors, null, 2));
+  describe('parser', () => {
+    it('should return', () => {
+      const selectors = parseSelector(selector);
+      expect(selectors).to.have.lengthOf(3);
+      expect(selectors[0].classes[0]).to.equal('mainWrapper');
+      expect(selectors[1].tagName).to.equal('SPAN');
+      expect(selectors[2].ids[0]).to.equal('id');
+    });
+  });
+});
+
