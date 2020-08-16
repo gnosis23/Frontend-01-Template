efd61d7abf43d46733de13fbbcc587e595665c47
Bohao Wang <bj050323@gmail.com>
2020-08-16 18:28:19 +0800

    add: week 19

---
+++ week19/toy-tool/app/templates/webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h' }]]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: {
+          loader: require.resolve('./src/custom-css-loader.js')
+        }
+      }
+      // {
+      //   test: /\.view/,
+      //   use: {
+      //     loader: require.resolve('./myloader.js')
+      //   }
+      // }
+    ]
+  },
+  optimization: {
+    minimize: false
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+}
