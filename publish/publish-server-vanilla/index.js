f256a4d14c66f8d461c21f2055a9fcbb4518736c
Bohao Wang <bj050323@gmail.com>
2020-08-26 23:22:20 +0800

    add: week 20 oauth

--- publish/publish-server-vanilla/index.js
+++ publish/publish-server-vanilla/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const axios = require('axios');
 
@@ -6,16 +7,44 @@ const server = http.createServer((req, res) => {
   if (req.url.match(/^\/auth/))
     return auth(req, res)
 
-  let matched = req.url.match(/filename=([^&]+)/);
-  let filename = (matched && matched[1]);
-  if (!filename) return;
-  console.log(filename);
-  let writeStream = fs.createWriteStream('../server/public/' + filename)
-  req.pipe(writeStream);
-  req.on('end', () => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('okay');
+  if (!req.url.match(/^\/?/)) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    return res.end('not found');
+  }
+
+
+  const options = {
+    hostname: 'api.github.com',
+    port: 443,
+    path: `/user`,
+    method: 'GET',
+    headers: {
+      Authorization: 'token ' + req.headers.token,
+      "User-Agent": "Awesome-Octocat-App"
+    }
+  };
+
+  const request = https.request(options, (response) => {
+    response.on('data', d => {
+      let result = d.toString();
+      console.log(result);
+    })
   });
+
+  request.on('error', e => console.error(e));
+
+  request.end();
+
+  // let matched = req.url.match(/filename=([^&]+)/);
+  // let filename = (matched && matched[1]);
+  // if (!filename) return;
+  // console.log(filename);
+  // let writeStream = fs.createWriteStream('../server/public/' + filename)
+  // req.pipe(writeStream);
+  // req.on('end', () => {
+  //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //   res.end('okay');
+  // });
 });
 
 server.listen(8081);
@@ -25,7 +54,7 @@ function auth(req, res) {
   console.log(code);
 
   let state = "abc123"
-  // FIXME: secret
+  // FIXME: 换回私钥
   let client_secret = ""
   let client_id = "Iv1.1f5811e6477e3e00"
   let redirect_uri = "http://localhost:8081/auth"
@@ -38,13 +67,20 @@ function auth(req, res) {
     client_id,
     redirect_uri
   }).then(response => {
-    let token = response.data.match(/access_token=([^&]+)/)[1]
-    console.log(token)
-    res.writeHead(200, {
-      'access_token': token,
-      'Content-Type': 'text/plain'
-    })
-    res.end('okay');
+    let match = response.data.match(/access_token=([^&]+)/)
+    if (match) {
+      const token = match[1];
+      res.writeHead(200, {
+        'access_token': token,
+        'Content-Type': 'text/html'
+      })
+      res.end(`<a href="http://localhost:8080/publish?token=${token}">publish</a>`);
+    } else {
+      res.writeHead(400, {
+        'Content-Type': 'text/plain'
+      })
+      res.end('error');
+    }
   }).catch(err => {
     console.error(err);
     res.end('error');
--- publish/publish-tool/publish.js
+++ publish/publish-tool/publish.js
@@ -6,39 +6,48 @@ const child_process = require('child_process');
 
 let packageName = './package';
 
+const redirect_url = encodeURIComponent('http://localhost:8081/auth')
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.1f5811e6477e3e00&redirect_uri=${redirect_url}&scope=user%3Aread&state=123abc`)
+const server = http.createServer((request, res) => {
+  console.log('real publish!!');
+  console.log(request.url);
+  let token = request.url.match(/token=([^&]+)/)[1];
 
-const options = {
-  host: 'localhost',
-  port: 8081,
-  path: '/?filename=package.zip',
-  method: 'POST',  headers: {
-    'Content-Type': 'application/octet-stream'
-  }
-};
+  const options = {
+    host: 'localhost',
+    port: 8081,
+    path: '/?filename=package.zip',
+    method: 'POST',
+    headers: {
+      'token': token,
+      'Content-Type': 'application/octet-stream'
+    }
+  };
 
 
-const req = http.request(options, res => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  const req = http.request(options, res => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  });
+
+  req.on(`error`, e => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  var archive = archiver('zip', {
+    zlib: { level: 9 }
+  });
+
+  archive.directory(packageName, false);
+
+  archive.finalize();
+
+  archive.pipe(req);
+
+  archive.on('end', () => {
+    req.end();
+  });
+
 });
-
-req.on(`error`, e => {
-  console.error(`problem with request: ${e.message}`);
-});
-
-var archive = archiver('zip', {
-  zlib: { level: 9 }
-});
-
-archive.directory(packageName, false);
-
-archive.finalize();
-
-archive.pipe(req);
-
-archive.on('end', () => {
-  req.end();
-  const redirect_url = encodeURIComponent('http://localhost:8081/auth')
-  child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.1f5811e6477e3e00&redirect_uri=${redirect_url}&scope=user%3Aread&state=123abc`)
-});
+server.listen(8080);
 
